66da382ae5fcc66029a4029005061a269e0e94d6
npeyre <90184564+npeyre@users.noreply.github.com>
2026-07-10 10:23:23 +0100

    Updated quote field

--- quotes_project/notable_quotes/models.py
+++ quotes_project/notable_quotes/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Quote(models.Model):
-    quote = models.TextField()
+    quote = models.TextField(unique=True)
     author = models.CharField(max_length=100)
 
     def __str__(self):

2149fc097a850a2be0d0e40b8fc5c9ed99cbd1f7
npeyre <90184564+npeyre@users.noreply.github.com>
2026-07-18 14:52:39 +0100

    Updated quote model

--- quotes_project/notable_quotes/models.py
+++ quotes_project/notable_quotes/models.py
@@ -1,13 +1,5 @@
 from django.db import models
 
-class Quote(models.Model):
-    quote = models.TextField(unique=True)
-    author = models.CharField(max_length=100)
-
-    def __str__(self):
-        return f'"{self.quote}" - {self.author}'
-    
-
 class Author(models.Model):
     name = models.CharField(max_length=100)
     born = models.IntegerField()
@@ -17,3 +9,17 @@ class Author(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Quote(models.Model):
+    quote = models.TextField(unique=True)
+
+    # Related name helps to retrieve all data (e.g. author.quotes.all() without using quote_set.all()
+    author = models.ForeignKey(
+        Author,
+        on_delete=models.CASCADE,
+        related_name="quotes"
+    )
+
+    def __str__(self):
+        return f'"{self.quote}" - {self.author}'

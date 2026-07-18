8bcb8f0abd3f52672ddad07dd6d401b5d7271a36
npeyre <90184564+npeyre@users.noreply.github.com>
2026-07-18 23:47:47 +0100

    Added author details view

--- quotes_project/notable_quotes/views.py
+++ quotes_project/notable_quotes/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
-from .models import Quote
+from django.shortcuts import render, get_object_or_404
+from .models import Quote, Author
 
 def home(request):
     # Retrieve all quotes for the database - Quote.objects.all()
     quotes = Quote.objects.all()
 
     return render(request, "notable_quotes/home.html", {"quotes": quotes})
+
+def author_details(request, author_id):
+    # getting an author object by id
+    author = get_object_or_404(Author, id=author_id)
+
+    return render(request, "notable_quotes/author.html", {"author": author})

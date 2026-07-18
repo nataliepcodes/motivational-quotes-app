2b689ead2d324d2ad07763e620a74969c7b30b6b
npeyre <90184564+npeyre@users.noreply.github.com>
2026-07-18 19:37:35 +0100

    Temporary fix to revert to prev version to load authors table

--- quotes_project/notable_quotes/models.py
+++ quotes_project/notable_quotes/models.py
@@ -14,11 +14,12 @@ class Quote(models.Model):
     quote = models.TextField(unique=True)
 
     # Related name helps to retrieve all data (e.g. author.quotes.all() without using quote_set.all()
-    author = models.ForeignKey(
+    author = models.CharField(max_length=100)
+    '''author = models.ForeignKey(
         Author,
         on_delete=models.CASCADE,
         related_name="quotes"
-    )
+    )'''
 
     def __str__(self):
         return f'"{self.quote}" - {self.author}'

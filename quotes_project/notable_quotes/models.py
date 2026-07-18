ae3122114a8788d6334ea82aa1b0f3f380b50c45
npeyre <90184564+npeyre@users.noreply.github.com>
2026-07-18 21:40:53 +0100

    Updated model for adding temp foreign key column to the database

--- quotes_project/notable_quotes/models.py
+++ quotes_project/notable_quotes/models.py
@@ -14,7 +14,8 @@ class Quote(models.Model):
     quote = models.TextField(unique=True)
 
     # Related name helps to retrieve all data (e.g. author.quotes.all() without using quote_set.all()
-    author = models.ForeignKey(
+    author = models.CharField(max_length=100)
+    author_temp = models.ForeignKey(
         Author,
         on_delete=models.CASCADE,
         related_name="quotes"

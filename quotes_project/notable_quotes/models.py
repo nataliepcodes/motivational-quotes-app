1d53c5e40275c93a72967a9eec396b60c1ac7420
npeyre <90184564+npeyre@users.noreply.github.com>
2026-07-18 20:01:40 +0100

    Updated author model

--- quotes_project/notable_quotes/models.py
+++ quotes_project/notable_quotes/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
-    born = models.IntegerField()
+    born = models.IntegerField(null=True, blank=True)
     country = models.CharField(max_length=100)
     famous_for = models.TextField()
 

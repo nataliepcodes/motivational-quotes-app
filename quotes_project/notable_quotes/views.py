f248612ce2533fb760c8f83d9731e977d80f3438
npeyre <90184564+npeyre@users.noreply.github.com>
2026-07-20 22:49:51 +0100

    Added category related sections

--- quotes_project/notable_quotes/views.py
+++ quotes_project/notable_quotes/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Quote, Author
+from .models import Quote, Author, Category
 
 def home(request):
     # Retrieve all quotes for the database - Quote.objects.all()
@@ -7,8 +7,15 @@ def home(request):
 
     return render(request, "notable_quotes/home.html", {"quotes": quotes})
 
+
 def author_details(request, author_id):
     # getting an author object by id
     author = get_object_or_404(Author, id=author_id)
 
     return render(request, "notable_quotes/author.html", {"author": author})
+
+
+def display_categories(request, category_name):
+    category = get_object_or_404(Category, id=category_name)
+
+    return render(request, "notable_quotes/category.html", {"category": category})

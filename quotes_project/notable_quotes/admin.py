1a5e452c058fe791899de317945dd484e8527f23
npeyre <90184564+npeyre@users.noreply.github.com>
2026-07-20 19:07:20 +0100

    Registered category with admin

--- quotes_project/notable_quotes/admin.py
+++ quotes_project/notable_quotes/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Quote, Author
+from .models import Quote, Author, Category
 
 admin.site.register(Quote)
 admin.site.register(Author)
+admin.site.register(Category)

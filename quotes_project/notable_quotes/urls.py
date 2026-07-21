34a6744cb691342181fa8a88276b1df3d8ea844e
npeyre <90184564+npeyre@users.noreply.github.com>
2026-07-21 23:52:24 +0100

    Added new path for all quotes

--- quotes_project/notable_quotes/urls.py
+++ quotes_project/notable_quotes/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("quotes/", views.all_quotes, name="all_quotes"),
     path("authors/<int:author_id>/", views.author_details, name="author_detail"),
     path("category/<str:category_name>/", views.display_categories, name="category_name"),
 ]
